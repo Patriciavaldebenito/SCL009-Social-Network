a7460ebe5c1f76274c622bd3e975b4ed83d959df
Patricia <patricia.valdebenito@usach.cl>
2019-05-30 18:14:56 -0400

    Se realizan los siguientes, en archivo server.js libreria, node express y funciones app.use - app.get -app.listen.    archivo auth.js evento DOM creacion de usuario y function firebase.auth().  archivo firebase-init.js inicializacion firestore.

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var express = require('express');
+// var exphbs = require('express-handlebars');
+
 var app = express();
 
+// app.engine('handlebars',exphbs({defaultLayout:'main'}));
+// app.set('view engine','handlebars');
+
+// static public in other movie youtube...
 app.use(express.static(__dirname + '/src'));
 
 app.get('/',function(request, response){
   response.sendFile(__dirname + '/index.html');
 });
 
+
+
+// app.post('/guardar',function(req,res){
+//   console.log(req.body);
+// });
+
 app.listen(3000, function(){
-  console.log('Server Express Ready!');
+  console.log('Server express ready!');
 });
+
