717e3f9baad67dd9d87b8b96dd9207da000a7891
Patricia <patricia.valdebenito@usach.cl>
2019-06-04 15:14:43 -0400

    En archivo validation.js se modifica la funciotn validationForm y se escriben 4 funciones para validad cada input del formulario

--- src/assets/controller/validation.js
+++ src/assets/controller/validation.js
@@ -1,38 +1,89 @@
-//  Validations  Functión createUser
+// Validation of registration fields
 
-//  validar = user / mail / edad / password /
-//                   largo mail.length <6 caracteres
-//                   pass contenga @
-//                   considerar que input edad tenga solo valor numerico
+  export const validationUser = (user) => {
 
-export const validationForm = (user, age, email, password) => {
-
-  if (user ==="" || typeof(user) === "undefined") {
-    console.log("false");
+  if (user ===""){
+    console.log("sin unsuario");
     return false;
   }
-  if (age <= 16 || typeof(age) === "undefined") {
-    console.log("false");
+  if ( typeof(user) === "undefined") {
+    console.log("usuario indefinido");
     return false;
   }
-  if (email ==="" || typeof(email) === "undefined") {
-    console.log("false");
-    return false;
-  }
-  if (password ==="" || password.length < 6 || typeof(password) === "undefined") {
-    console.log("false");
-    return false;
-  }
-  console.log("true");
+  console.log("usuario OK")
   return true;
-}
+  
+ }
+ 
+ export const validationAge = (age) => {
+
+  if (age <=16){
+    console.log("aun no tienes la edad ");
+    return false;
+  }
+  if ( typeof(age) === "undefined") {
+    console.log("edad indefinido");
+    return false;
+  }
+
+  console.log("edad OK")
+  return true;
+ 
+ }
+
+ export const validationEmail = (email) => {
+
+  if (email === ""){
+    console.log("sin correo ");
+    return false;
+  }
+  if ( typeof(email) === "undefined") {
+    console.log("correo indefinido");
+    return false;
+  }
+
+  console.log("correo OK")
+  return true;
+ 
+ }
+
+ export const validationPassword = (password) => {
+
+  if (password === ""){
+    console.log("sin password ");
+    return false;
+  }
+  if ( typeof(password) === "undefined") {
+    console.log("password indefinido");
+    return false;
+
+  } if ( password.length < 6 ) {
+    console.log("no cumple el largo de contraseña");
+    return false;
+  }
 
 
-export const validateEmail = (email) => {
+  console.log("password OK")
+  return true;
+ 
+ }
+
+
+export const validationExpRegEmail = (email) => {
 
   // var re = /\S+@\S+/;
   //La prueba de validación de correo electrónico simple y futura será verificar la presencia del símbolo @
   var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
   return re.test(email);
 
+}
+
+export const validationForm = (user, age, email, password) => {
+  if ( validationUser(user) === false || validationAge(age) ===false|| validationEmail(email) === false || validationPassword(password) ===false || !validationExpRegEmail(email)){
+    console.log("Un campo no cumple el testeo")
+    return false;
+  }
+
+  console.log("true test Ok");
+  return true;
 }
--- test/validation.spec.js
+++ test/validation.spec.js
@@ -1,11 +1,15 @@
-import { validationForm } from "../src/assets/controller/validation.js";
+import { validationUser } from "../src/assets/controller/validation.js";
+import {  } from "../src/assets/controller/validation.js";
+import {  } from "../src/assets/controller/validation.js";
+import {  } from "../src/assets/controller/validation.js";
 
-describe('validationForm', () => {
+
+describe('', () => {
   it('debería retornarnos false si falta un campo', () => {
-    expect(validationForm(" ","16","abb@hh.cl","123456")).toBe(false)
-    expect(validationForm("ana","16","abb@hh.cl","123")).toBe(false)
-    expect(validationForm("ana","17","abb@hh.cl","123456")).toBe(true)
-    expect(validationForm("ana","17","abb@hh.cl","12345678")).toBe(true)
+    expec(" ","16","abb@hh.cl","123456")).toBe(false)
+    expect(("ana","16","abb@hh.cl","123")).toBe(false)
+    expect(("ana","17","abb@hh.cl","123456")).toBe(true)
+    expect(("ana","17","abb@hh.cl","12345678")).toBe(true)
 
 
   });
