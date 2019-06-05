210652dff662256838dcede2c8b80122f3fe9fd6
Patricia <patricia.valdebenito@usach.cl>
2019-06-05 01:02:07 -0400

    Se modifica archivo app.js borrando su contenido, archivo tempateHome.js es renombrado como archivo templateHome.js, archivo init.js se adhiere funcion initiationFirebase, se agrega contenido dinamico funciones: templateRegistre y templateHome,import /export

--- src/assets/js/firebase_auth.js
+++ src/assets/js/firebase_auth.js
@@ -5,7 +5,7 @@ export const createUser = (user, age, email, password) => {
   console.log(email);
   console.log(password);
 
-  firebase.auth().createUserWithEmailAndPassword(email, password).then().catch(function(error) {
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
