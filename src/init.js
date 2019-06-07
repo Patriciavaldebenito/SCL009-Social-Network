3056c36dc059d6e48685de556429d4daac346a2c
Patricia <patricia.valdebenito@usach.cl>
2019-06-07 16:14:40 -0400

    Se modific funcion login con funcion verificar mail

--- src/init.js
+++ src/init.js
@@ -1,10 +1,13 @@
 // Inicializando 
 import { initRouter } from './route.js';
 import { initiationFirebase } from './assets/js/firebase_init.js';
+import { observer } from './assets/js/firebase_auth.js'
 
 const init = () => {
+  
+  observer();
   initRouter();
   initiationFirebase();
-  
+ 
 };
 window.addEventListener('load',init);
