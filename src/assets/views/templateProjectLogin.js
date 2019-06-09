c1f5d101c5b103a3a928b64f64fd7d7f17dded6b
gabagonzalez <gaba2801@gmail.com>
2019-06-09 17:17:43 -0400

    button back login

--- src/assets/views/templateProjectLogin.js
+++ src/assets/views/templateProjectLogin.js
@@ -1,64 +1,59 @@
-import { loginGoogle } from "./../js/firebase_auth.js";
-import { validationFormSignLogin } from "../controller/validation.js";
-import { signLogin } from "../js/firebase_auth.js";
-import { aparece } from "../js/firebase_auth.js";
+import {
+  loginGoogle
+} from "./../js/firebase_auth.js";
+import {
+  validationFormSignLogin
+} from "../controller/validation.js";
+import {
+  signLogin
+} from "../js/firebase_auth.js";
+import {
+  aparece
+} from "../js/firebase_auth.js";
 
 // import { loginGoogle } from "./../js/firebase_auth.js"; **** revisar ruta ****
 // import {} from "";
 
 export const templateLogin = () => {
   document.getElementById("root").innerHTML = `
-                                                <div id="login" class="modal">
-                                                  <div class="input-field">
+                                                <div class="modal" id="modal-login">
+                                                  <div class="modal-content">
+                                                    <h3>Inicia Sesión</h3>
                                                     <form id="login-form">
-                                                      <div class="row"
-                                                        <div class="col input-field">
-                                                          <h3>Inicia Sesión</h3>
+                                                        <div class="row input-field">
+                                                        <div class="col"
+                                                          <input type="email" placeholder="Correo" id="login-email" required />
+                                                          <label for="login-email"></label>
                                                         </div>
                                                       </div>
-
-                                                      <div class="row"
-                                                        <div class="col input-field">
-                                                        <input type="email" placeholder="Correo" id="login-email" required />
-                                                          
-                                                        </div>
-                                                      </div>
-
-                                                      <div class="row"
-                                                        <div class="col input-field">
-                                                        <input type="password" placeholder="Contraseña" id="login-password" required />
-                                                        </div>
-                                                      </div>
-
-                                              
                                                       <div class="row input-field">
-                                                        <div class="row icol">
+                                                        <div class="col"
+                                                          <input type="password" placeholder="Contraseña" id="login-password" required />
+                                                          <label for="login-password"></label>
+                                                        </div>
+                                                      </div>
+                                                      <div class="row input-field">
+                                                        <div class="col"
                                                           <button id="btnLogin" class="btn-create">Ingresa</button>
                                                         </div>
                                                       </div>
-
-                                                      <div class="row"
-                                                        <div class="col input-field">
+                                                      <div class="row input-field">
+                                                        <div class="col"
                                                           <button class="btn-google" type="button" id="buttonGoogleLogin">Acceso con Google</button>
                                                         </div>
                                                       </div>
-
+                                                      <div class="row input-field">
+                                                        <div class="col"
+                                                          <button id="back" type="button"></button>
+                                                        </div>
+                                                      </div>
                                                     </form>
                                                   </div>
-<<<<<<< HEAD
-                                                  <br>
-                                                  <button id="back" type="button"></button>
-                                                </div>
-                                               
-                                               `;
-=======
                                                 </div>`;
->>>>>>> 22288b9c4e98014dc6588bfe5f8d91185202a48c
-//<label for="login-email"></label>
-//<label for="login-password"></label>
+
   // 1. En Login click en btnLogin
   document.getElementById("btnLogin").addEventListener("click", () => {
-  
+
     // valores desde imput text para function singLogin
     let email = document.getElementById('login-email').value;
 
@@ -71,28 +66,26 @@ export const templateLogin = () => {
     // para logearse los datos ingresados por el usuario deben estar completos y pasar las validaciones 
     // y ademas el user.emailverified deben retornar true
     // asi se ejecuta la fx singLogin y se muestra el muro
-   
-       if(resSignLogin){
-        signLogin(email, password);
-        console.log("logeado / parametros validados");
-     
+
+    if (resSignLogin) {
+      signLogin(email, password);
+      console.log("logeado / parametros validados");
+
     } else {
       console.log("NO    createUser / parametros validos");
     }
-
+    
 
   });
 
-  document.getElementById('back').addEventListener('click', () => {
-    templateHome();
-  });
+
 
   // 2. En login click en btn google
   document.getElementById("buttonGoogleLogin").addEventListener("click", () => {
     console.log("presionaste boton google en Home!");
-     loginGoogle();
-    
-   
+    loginGoogle();
+
+
   });
 
 };
