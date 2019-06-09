0addfdc7d4a01ac90e09c4e8f92f59f7f56af779
gabagonzalez <gaba2801@gmail.com>
2019-06-08 23:38:53 -0400

    implementando iteraciones Hu1

--- src/assets/views/templateAboutRegistre.js
+++ src/assets/views/templateAboutRegistre.js
@@ -6,41 +6,36 @@ import { templateHome } from "./templateHome.js";
 
 export const templateRegistre = () => {
   let containerRegistre = document.getElementById("root");
-  let contenidoRegistre = ` 
+  let contenidoRegistre = `<br>
   <div class="modal" id="modal-signup" >
     <div class="modal-content">
       <h3>Crea tu cuenta</h3>
       <form id="signup-form">
         <div class="input-field">
-          <input type="text"  id="signup-user"/>
-          <label for="signup-user">usuario</label>
+          <input type="text" placeholder="Nombre" id="signup-user"/>
+          <label for="signup-user"></label>
         </div>
         <div class="input-field">
-          <input type="number"  id="signup-age"/>
-          <label for="signup-age">edad</label>
+          <input type="number" placeholder="Edad" id="signup-age"/>
+          <label for="signup-age"></label>
         </div>
         <div class="input-field">
-          <input type="email" placeholder="Ingresa tu correo" id="signup-email"/>
+          <input type="email" placeholder="Correo" id="signup-email"/>
           <label for="signup-email"></label>
         </div>
         <div class="input-field">
-          <input type="password" placeholder="Ingresa tu contraseña" id="signup-password"/>
+          <input type="password" placeholder="Contraseña" id="signup-password"/>
           <label for="signup-password"></label>
         </div>
         <br>
         <button class="btn-create" id="btnProbando" >Crear</button>
-        <button id="back" type="button"></button>
+        
       </form>
     </div>
-  </div> 
-<<<<<<< HEAD
-  <button class="btn-google" type="button" id="buttonGoogle">Acceso con Google</button>
-  
-  `;
-=======
-  <button class="btn-google" type="button" id="buttonGoogleRegistre">Acceso con Google</button>`;
->>>>>>> 116675b947a4b199b4d7f758d568bfbb90dc4392
-
+    <button class="btn-google" type="button" id="buttonGoogle">Acceso con Google</button>
+    <br>
+    <button id="back" type="button"></button>
+  </div>  `;
   containerRegistre.innerHTML = contenidoRegistre;
 
   // 1. En registre click en btn probando ****cambiar name gaba??¡¡
@@ -68,7 +63,7 @@ export const templateRegistre = () => {
   });
   
   // 2. En registre click en btn google
-  document.getElementById("buttonGoogleRegistre").addEventListener("click", () => {
+  document.getElementById("buttonGoogle").addEventListener("click", () => {
     console.log("presionaste boton google en Home!");
 
      loginGoogle();
